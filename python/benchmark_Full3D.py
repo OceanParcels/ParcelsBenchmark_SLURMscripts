804c321304f352a6e181bab259d4cdd3039663b3
Christian Kehl <c.kehl@uu.nl>
2022-05-13 12:44:12 +0200

    updated full-3D script and added scipy mode to galapagos

--- python/benchmark_Full3D.py
+++ python/benchmark_Full3D.py
@@ -737,29 +737,15 @@ if __name__ == "__main__":
                                  pclass=plastic_ptype[(args.compute_mode).lower()],   # the type of particles (JITParticle or ScipyParticle)  # plastic_particle
                                  lon= lon_additional,          # a vector of release longitudes
                                  lat= lat_additional,          # a vector of release latitudes
-                                 # time = (np.ones(lon_additional.shape) * np.datetime64('%s-%s-05' % (str(year), '01'))).astype(dtype=np.datetime64),
                                  time=np.datetime64('%s-%s-05' % (str(year), '01')),
                                  depth = z_additional,         # a vector of release depth values
                                  repeatdt=delta(minutes=repeatRateMinutes) if not cleanrun else None,
                                  idgen=idgen,
                                  c_lib_register=c_lib_register)
     if not cleanrun:
-        # if pset_type != 'nodes':
-        #     psetA = ParticleSet(fieldset=fieldset, pclass=plastic_ptype[(args.compute_mode).lower()],
-        #                         lon=lon_release,
-        #                         lat=lat_release,
-        #                         depth=z_release,
-        #                         # time=(np.ones(lon_release.shape) * np.datetime64('%s-%s-05' % (str(year), '01'))).astype(dtype=np.datetime64),
-        #                         time=np.datetime64('%s-%s-05' % (str(year), '01')),
-        #                         idgen=idgen,
-        #                         c_lib_register=c_lib_register)
-        #     pset.add(psetA)
-        # else:
-        if True:
-            time_field = np.array([np.datetime64('%s-%s-05' % (str(year), '01')), ] * lon_release.shape[0], dtype=np.datetime64)
-            # pdata = np.concatenate( (lonlat_field, time_field), axis=1 )
-            pdata = {'lon': lon_release, 'lat': lat_release, 'depth': z_release, 'time': time_field}
-            pset.add(pdata)
+        time_field = np.array([np.datetime64('%s-%s-05' % (str(year), '01')), ] * lon_release.shape[0], dtype=np.datetime64)
+        pdata = {'lon': lon_release, 'lat': lat_release, 'depth': z_release, 'time': time_field}
+        pset.add(pdata)
 
     """ Kernel + Execution"""
     postProcessFuncs = None
